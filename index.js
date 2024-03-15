25c1758ad38a8efb520cb3c931dc7f22f159206a
Niko Vänttilä <niko.vanttila@gmail.com>
2024-03-15 14:26:51 +0200

    3.5: Phonebook backend step 5

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ let persons = [
     }
 ]
 
+app.use(express.json())
+
 app.get('/info', (request, response) => {
     const date = new Date()
     response.send(
@@ -52,6 +54,26 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 10000),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
   
 const PORT = 3001
