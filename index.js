4e096b291c7946f892e4a14b1d8bc28d791f4a97
Niko Vänttilä <niko@MacBook.local>
2024-03-18 10:11:10 +0200

    3.7: Phonebook backend step 7

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require('express')
+var morgan = require('morgan')
 const app = express()
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/info', (request, response) => {
     const date = new Date()
